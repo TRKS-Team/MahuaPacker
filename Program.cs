6b1a0db28d90a245b211dd5b7c60d82af3f313fd
Cyl18 <cyl18a@gmail.com>
2018-12-08 17:45:17 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahuaPacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var platformNames = new[] { "CleverQQ", "MPQ", "CQP", "QQLight" };
+            foreach (var platformName in platformNames)
+            {
+                var results = Directory.GetDirectories(".", platformName, SearchOption.AllDirectories)
+                    .Where(dir => dir.ToLower().Contains("bin")).ToArray();
+                if (results.Length == 0) continue;
+                if (results.Length > 1) throw new DirectoryNotFoundException(
+                    $"Found multiple folder that meets the requirement: {string.Join(", ", results)}.");
+
+                Console.WriteLine($"Compressing: {platformName}");
+                ZipFile.CreateFromDirectory(results.First(), $"{platformName}.zip");
+            }
+        }
+    }
+}
